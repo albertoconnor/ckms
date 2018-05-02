0ac4ce2ed5dfc31270f66f9abb1e1a17d265d69c
Albert O'Connor <amjoconn@gmail.com>
2018-05-02 07:27:45 -0400

    UNTESTED: split files using mp3splt

--- dubber.py
+++ dubber.py
@@ -1,47 +1,11 @@
 import os
 import datetime
-
-from dateutil.parser import parse
+import shutil
+import subprocess
 
 from pydub import AudioSegment
 
-from schedules import Show, time_to_key, key_to_datetime, read_schedule
-
-
-def simple_loader(filename):
-    return AudioSegment.from_mp3(filename)
-
-
-def minutes_to_microseconds(minute):
-    return minute * 60 * 1000
-
-
-def dub_edits(edits, segments_loader):
-    output_seg = None
-    for edit in edits:
-        source_seg = segments_loader(edit['filename'])
-        start, end = edit.get('start'), edit.get('end')
-
-        if start is None and end is None:
-            cut_seg = source_seg
-        elif end is None:
-            cut_seg = source_seg[start:]
-        elif start is None:
-            cut_seg = source_seg[:end]
-        else:
-            cut_seg = source_seg[start:end]
-
-        if output_seg is None:
-            output_seg = cut_seg
-        else:
-            output_seg += cut_seg
-
-    return output_seg
-
-
-def dub_show(show, edits):
-    output = dub_edits(edits, simple_loader)
-    output.export(f'output/{show.name}.mp3')
+from schedules import time_to_key, key_to_datetime, read_schedule
 
 
 def index_from_filelist(filelist):
@@ -158,7 +122,98 @@ def shows_and_edits_from_schedule(schedule):
     return ret
 
 
+# pydubber implementation
+def simple_loader(filename):
+    return AudioSegment.from_mp3(filename)
+
+
+def minutes_to_microseconds(minute):
+    return minute * 60 * 1000
+
+
+def dub_edits(edits, segments_loader):
+    output_seg = None
+    for edit in edits:
+        source_seg = segments_loader(edit['filename'])
+        start, end = edit.get('start'), edit.get('end')
+
+        if start is None and end is None:
+            cut_seg = source_seg
+        elif end is None:
+            cut_seg = source_seg[start:]
+        elif start is None:
+            cut_seg = source_seg[:end]
+        else:
+            cut_seg = source_seg[start:end]
+
+        if output_seg is None:
+            output_seg = cut_seg
+        else:
+            output_seg += cut_seg
+
+    return output_seg
+
+
+def dub_show(show, edits):
+    output = dub_edits(edits, simple_loader)
+    output.export(f'output/{show.name}.mp3')
+
+
+# mp3split implementation
+def edit_to_split(edit):
+    filename, start, end = edit
+    return(
+        filename,
+        '{}.0'.format(start),
+        '{}.0'.format(end),
+    )
+
+
+def mp3split(split, output_path):
+    filename, start, end = split
+
+    ret = subprocess.call(
+        'mp3splt -d {} {} {} {}'.format(
+            output_path,
+            filename,
+            start,
+            end,
+        ),
+        shell=True
+    )
+
+    if ret != 0:
+        print('Unexpected error while spliting.')
+
+
+def do_split(show, edits):
+    dirname = '{name}-{instace_id}'.format(**show)
+    output_path = os.path.join('output', dirname)
+    os.makedirs(output_path)
+
+    for edit in edits:
+        filename, start, end = edit
+        if start == 0 and end == 120:
+            shutil.copy(
+                os.path.join('test_data', filename),
+                os.path.join(output_path, filename)
+            )
+        else:
+            split = edit_to_split(edit)
+            mp3split(split, output_path)
+
+    return output_path
+
+
 if __name__ == '__main__':
     schedule = read_schedule('2018-04-02T00-00.json')
     shows_and_edits = shows_and_edits_from_schedule(schedule)
     print(shows_and_edits)
+
+    test_show_and_edits = [(
+        {'key': '2018-04-03T16-00', 'details': {'instance_id': 28511, 'name': 'Mazaj Show', 'start_key': '2018-04-03T16-00', 'end_key': '2018-04-03T19-30'}},
+        [
+            ('dump_end_Apr03_2018_18_00.mp3', 0, 120),
+            ('dump_end_Apr03_2018_20_00.mp3', 0, 90),
+        ]
+    )]
