34d26e5833b4d5af39ddd735f7a15368c488f5a8
Albert O'Connor <amjoconn@gmail.com>
2019-01-18 21:15:53 -0500

    Load sentry information form .env and trim history.

--- main.py
+++ main.py
@@ -1,14 +1,38 @@
+import datetime
 import os
 import json
 
 import click
 import sentry_sdk
+from dotenv import load_dotenv
 
 import schedules
 import dubber
 
 
-sentry_sdk.init("https://dc5cab9a48f84900a9decb152a64cfb2@sentry.io/1369605")
+load_dotenv(dotenv_path='.env', verbose=True)
+
+
+sentry_dns = os.getenv('SENTRY_DNS', '')
+if sentry_dns == '':
+    print(
+        'No sentry_dns found, no logging available.'
+        ' Define a .env file here with a SENTRY_DNS entry.'
+    )
+sentry_sdk.init(sentry_dns)
+
+
+def trim_history(history):
+    '''
+    We don't want the history file growing indefinitately, so if we exclude
+    show older than eight weeks, we should be ok.
+    '''
+    eight_weeks_ago = datetime.datetime.now() - datetime.timedelta(days=56)
+    new_history = dict([
+        (key, history[key]) for key in history
+        if schedules.key_to_datetime(key) > eight_weeks_ago
+    ])
+    return new_history
 
 
 @click.command()
@@ -36,12 +60,11 @@ def cli(record_path, schedule_url, schedule_path, history_path):
         with open(history_path, 'r') as f:
             history = json.loads(f.read())
 
-    print(schedules_dict.keys())
     if len(schedules_dict.keys()) > 2:
         first_key = schedules_dict.keys()[0]
         start_key, path = first_key
         sentry_sdk.capture_message('Deleteing schedule file {}'.format(path))
-        os.remove(path) # the underlying schedule file
+        os.remove(path)  # the underlying schedule file
         del schedules_dict[first_key]
 
     schedules_list = [schedules_dict[key] for key in schedules_dict]
@@ -60,6 +83,8 @@ def cli(record_path, schedule_url, schedule_path, history_path):
         filename = dubber.dub_show(show, edits, record_path)
         shows_written.append(filename)
         history[show['key']] = dict(show=show, flename=filename)
+
+    history = trim_history(history)
 
     history_json = json.dumps(history)
     with open(history_path, 'w') as f:
